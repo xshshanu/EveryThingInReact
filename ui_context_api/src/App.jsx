d504802e08e2cedb55208ef0c2c81d5b6c6c260b
Shanu <mdshahbuddin9504@gmail.com>
2023-12-22 17:08:40 +0530

    Mini Context Api Project

---
+++ ui_context_api/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import Login from './components/Login';
+import Profile from './components/Profile';
+import UserContextProvider from './context/UserContextProvider';
+
+function App() {
+
+  return (
+    <UserContextProvider>
+      <Profile />
+      <Login />
+    </UserContextProvider>
+  )
+}
+
+export default App
---
+++ ui_context_api/src/context/UserContextProvider.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import Usercontext from './UserContext';
+
+const UserContextProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+
+  return (
+    <Usercontext.Provider value={{user, setUser}}>
+        {children}
+    </Usercontext.Provider>
+  )
+}
+
+export default UserContextProvider
